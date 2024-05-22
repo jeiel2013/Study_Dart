534dc32b0857c514bf2a1105f5692a757d7fedb3
jeiel2013 <contatojeiel2013@gmail.com>
2024-05-22 11:21:14 -0300

    first commit: reloaded

--- primeiroprojeto/lib/main.dart
+++ primeiroprojeto/lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Página Inicial Teste Jeiel'),
     );
   }
 }
@@ -84,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Meu título"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
