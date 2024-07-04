2c16ae94011960c32ed486d8a9fa8a7a0720f4a0
jeiel2013 <contatojeiel2013@gmail.com>
2024-07-04 18:35:25 -0300

    att 07/07

--- teste/lib/main.dart
+++ teste/lib/main.dart
@@ -8,101 +8,68 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    print('executando metodo build MyApp');
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 231, 0, 135)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Demo Page Jeiel'),
+      home: ListaContatos(
+        contatos: const [
+          'Rodrigo',
+          'Lara',
+          'Jeiel',
+          'Pedro',
+          'Fernanda',
+          'Isabela',
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ListaContatos extends StatelessWidget {
+  ListaContatos({required this.contatos});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<String> contatos;
 
   @override
   Widget build(BuildContext context) {
-    final listaNomes = [
-      'Rodrigo',
-      'Paulo',
-      'Joana',
-      'Rafael',
-      'Felipe',
-      'Lara',
-    ];
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Lista de Contatos'),
+        leading: const Icon(Icons.contact_phone),
+        centerTitle: true,
       ),
       body: ListView.builder(
-        itemBuilder: (context, index) => ListTile(
-          leading: Icon(Icons.person),
-          title: Text(listaNomes[index]),
-          trailing: Container(
-            width: 60,
-            child: Row(
-              children: [
-                Icon(Icons.edit),
-                SizedBox(width: 4),
-                Icon(Icons.delete),
-              ],
-            ),
-          ),
-        ),
-        itemCount: listaNomes.length,
-        ),
-      // body: Center(
-      //   child: Column(children: [
-      //     const Text('Meus dois containers vermelhos!',
-      //         style: TextStyle(
-      //           fontSize: 22,
-      //           fontWeight: FontWeight.bold,
-      //           color: Colors.red,
-      //         )),
-      //     Row(
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       children: <Widget>[
-      //         Container(height: 80, width: 80, color: Colors.red),
-      //         const SizedBox(width: 12),
-      //         Container(height: 80, width: 80, color: Colors.red)
-      //       ],
-      //     ),
-      //     SizedBox(height: 50),
-      //     Row(
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       children: [
-      //         Icon(Icons.access_alarm, size: 48, color: Colors.blue),
-      //         Icon(Icons.account_circle, size: 60, color: Colors.red),
-      //       ],
-      //     )
-      //   ]),
-      // ),
+        itemBuilder: (context, index) => LinhaContato(contato: contatos[index]),
+        itemCount: contatos.length,
+      ),
+    );
+  }
+}
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+class LinhaContato extends StatelessWidget {
+  const LinhaContato({required this.contato});
+
+  final String contato;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.person_outline),
+      title: Text(contato),
+      trailing: Container(
+        width: 60,
+        child: Row(
+          children: [
+            Icon(Icons.edit),
+            SizedBox(width: 6),
+            Icon(Icons.delete),
+          ],
+        ),
+      ),
     );
   }
 }
