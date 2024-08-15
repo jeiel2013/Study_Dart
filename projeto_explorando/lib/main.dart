c794d678b7dd95b72d44256a50337b05dec905a7
jeiel2013 <contatojeiel2013@gmail.com>
2024-08-15 13:39:12 -0300

    Eventos & update height screen

--- projeto_explorando/lib/main.dart
+++ projeto_explorando/lib/main.dart
@@ -37,163 +37,196 @@ class Home extends StatelessWidget {
             padding: EdgeInsets.only(
                 top: 80), // Ajusta o padding para não sobrepor a barra fixa
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                // Espaço
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 60),
-                      child: Container(
-                        width: 350,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 228, 222, 222),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: TextField(
-                          controller: _controller,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.all(16),
-                              labelText: 'Pesquise espaços e eventos',
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelStyle: TextStyle(
-                                color: Preto,
-                                fontSize: 16,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.bold,
-                              )),
-                          style: TextStyle(
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60),
+                    child: Container(
+                      width: 350,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 228, 222, 222),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(16),
+                            labelText: 'Pesquise espaços e eventos',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelStyle: TextStyle(
                               color: Preto,
+                              fontSize: 16,
                               fontFamily: 'Montserrat',
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold),
-                        ),
+                              fontWeight: FontWeight.bold,
+                            )),
+                        style: TextStyle(
+                            color: Preto,
+                            fontFamily: 'Montserrat',
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold),
                       ),
                     ),
-                    // Stories
-                    Padding(
-                      padding: const EdgeInsets.only(top: 13),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Padding(
-                                padding: EdgeInsets.only(top: 75, left: 20)),
-                            StoryCircle(imageUrl: 'assets/S1.jpg'),
-                            StoryCircle(imageUrl: 'assets/S2.jpg'),
-                            StoryCircle(imageUrl: 'assets/S3.jpg'),
-                            StoryCircle(imageUrl: 'assets/S4.jpg'),
-                            StoryCircle(imageUrl: 'assets/S5.jpg'),
-                            StoryCircle(imageUrl: 'assets/S6.jpg'),
-                            StoryCircle(imageUrl: 'assets/S7.jpg'),
-                            StoryCircle(imageUrl: 'assets/S8.jpg'),
-                            StoryCircle(imageUrl: 'assets/S9.jpg'),
-                            StoryCircle(imageUrl: 'assets/S10.jpg'),
-                          ],
-                        ),
-                      ),
-                    ),
-                    // Espaços
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15),
-                      child: Column(
+                  ),
+                  // Stories
+                  Padding(
+                    padding: const EdgeInsets.only(top: 13),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Container(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 22),
-                              child: Text(
-                                "Espaços com base em suas preferências",
-                                style: TextStyle(
-                                  color: Preto,
-                                  fontFamily: "Montserrat",
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 21,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Padding(padding: EdgeInsets.only(left: 12)),
-                                TopicsImg(
-                                  imageUrl: 'assets/ibi.jpg',
-                                  texto: 'Pico da Ibituruna',
-                                ),
-                                TopicsImg(
-                                  imageUrl: 'assets/atiaia.jpg',
-                                  texto: 'Teatro Atiaia',
-                                ),
-                                TopicsImg(
-                                  imageUrl: 'assets/nelson.jpg',
-                                  texto: 'Centro Cultural Nelson Mandela',
-                                ),
-                                TopicsImg(
-                                  imageUrl: 'assets/nelson.jpg',
-                                  texto: 'Centro Cultural Nelson Mandela',
-                                ),
-                                TopicsImg(
-                                  imageUrl: 'assets/nelson.jpg',
-                                  texto: 'Centro Cultural Nelson Mandela',
-                                ),
-                              ],
-                            ),
-                          )
+                          Padding(padding: EdgeInsets.only(top: 75, left: 20)),
+                          StoryCircle(imageUrl: 'assets/S1.jpg'),
+                          StoryCircle(imageUrl: 'assets/S2.jpg'),
+                          StoryCircle(imageUrl: 'assets/S3.jpg'),
+                          StoryCircle(imageUrl: 'assets/S4.jpg'),
+                          StoryCircle(imageUrl: 'assets/S5.jpg'),
+                          StoryCircle(imageUrl: 'assets/S6.jpg'),
+                          StoryCircle(imageUrl: 'assets/S7.jpg'),
+                          StoryCircle(imageUrl: 'assets/S8.jpg'),
+                          StoryCircle(imageUrl: 'assets/S9.jpg'),
+                          StoryCircle(imageUrl: 'assets/S10.jpg'),
                         ],
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 115),
-                              child: Text(
-                                "Navegue por categoria",
-                                style: TextStyle(
-                                  color: Preto,
-                                  fontFamily: "Montserrat",
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 21,
-                                ),
+                  ),
+                  // Espaços
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 22),
+                            child: Text(
+                              "Espaços com base em suas preferências",
+                              style: TextStyle(
+                                color: Preto,
+                                fontFamily: "Montserrat",
+                                fontWeight: FontWeight.bold,
+                                fontSize: 21,
                               ),
                             ),
                           ),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Padding(padding: EdgeInsets.only(left: 12)),
-                                TopicsImg(
-                                  imageUrl: 'assets/arte.jpg',
-                                  texto: 'Arte',
-                                ),
-                                TopicsImg(
-                                  imageUrl: 'assets/teatro.jpg',
-                                  texto: 'Teatro',
-                                ),
-                                TopicsImg(
-                                  imageUrl: 'assets/musica.jpg',
-                                  texto: 'Música',
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 12)),
+                              TopicsImg(
+                                imageUrl: 'assets/ibi.jpg',
+                                texto: 'Pico da Ibituruna',
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/atiaia.jpg',
+                                texto: 'Teatro Atiaia',
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/nelson.jpg',
+                                texto: 'Centro Cultural Nelson Mandela',
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/nelson.jpg',
+                                texto: 'Centro Cultural Nelson Mandela',
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/nelson.jpg',
+                                texto: 'Centro Cultural Nelson Mandela',
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 115),
+                            child: Text(
+                              "Navegue por categoria",
+                              style: TextStyle(
+                                color: Preto,
+                                fontFamily: "Montserrat",
+                                fontWeight: FontWeight.bold,
+                                fontSize: 21,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 12)),
+                              TopicsImg(
+                                imageUrl: 'assets/arte.jpg',
+                                texto: 'Arte',
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/teatro.jpg',
+                                texto: 'Teatro',
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/musica.jpg',
+                                texto: 'Música',
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 165),
+                            child: Text(
+                              "Próximos eventos",
+                              style: TextStyle(
+                                color: Preto,
+                                fontFamily: "Montserrat",
+                                fontWeight: FontWeight.bold,
+                                fontSize: 21,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 12),
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/diversidade.jpg',
+                                texto: 'Festival Diversidade',
+                              ),
+                              TopicsImg(
+                                imageUrl: 'assets/sarau.jpg',
+                                texto: 'Sarau do Deck',
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -278,7 +311,7 @@ class TopicsImg extends StatelessWidget {
           print('Tópico clicado');
         },
         child: Padding(
-          padding: const EdgeInsets.only(top: 15),
+          padding: const EdgeInsets.only(top: 15, bottom: 20),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(16.0),
             child: Stack(
